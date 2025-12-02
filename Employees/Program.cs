5ca83ed0f6f7b97c98953f94307c78545d28b104
devops.arman <paplix.dev@gmail.com>
2025-12-02 15:29:54 +0800

    Basic configuration

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Employees.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+var jwtsettings = builder.Configuration.GetSection("JwtSettings");
+var key= Encoding.UTF8.GetBytes(jwtsettings["SecretKey"] ?? string.Empty);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
 
 app.Run();
