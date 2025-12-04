2646d2084340a2fe69459af6f62e8bd29897d332
devops.arman <paplix.dev@gmail.com>
2025-12-04 12:54:34 +0800

    Adding Views

--- Employees/Program.cs
+++ Employees/Program.cs
@@ -62,6 +62,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+
 
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
